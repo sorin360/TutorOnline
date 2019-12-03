cd798ce774613102a8ee4ce16349aed7cc030939
slica <sorin.lica@atoss.com>
2019-12-03 07:29:47 +0200

    Initial Commit

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Lica Sorin on 03/12/2019.
+//  Copyright © 2019 Lica Sorin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
