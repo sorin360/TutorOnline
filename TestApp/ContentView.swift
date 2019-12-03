232bc0a77dece1857e098b6cbf130403b83df41d
slica <sorin.lica@atoss.com>
2019-12-03 08:23:41 +0200

    Create and register user

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -7,12 +7,66 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
+    
+    @State var presentLogin = false
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button("Logout") {
+                try! Auth.auth().signOut()
+                self.presentLogin = true
+            }
+            Text("Hello, World!").sheet(isPresented: $presentLogin, onDismiss: {
+                if Auth.auth().currentUser == nil {
+                    self.presentLogin = true
+                }
+            }) {
+                RegisterView()
+            }.onAppear {
+                if Auth.auth().currentUser == nil {
+                    self.presentLogin = true
+                }
+                
+            }
+        }
+        
     }
 }
+
+
+struct RegisterView: View {
+    
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Name", text: $name)
+            TextField("Email", text: $email)
+            TextField("Password", text: $password)
+            Button("Register") {
+                Auth.auth().createUser(withEmail: self.email, password: self.password) { authResult, error in
+                    guard error == nil else {
+                        print("Create user error: \(error!)")
+                        return
+                    }
+                    let ref = Database.database().reference()
+                    ref.child("users").child(authResult?.user.uid ?? "").updateChildValues(["name":self.name, "email":self.email, "password":self.password], withCompletionBlock: { (error, databaseReference) in
+                        guard error == nil else {
+                            print("Add user in database error: \(error!)")
+                            return
+                        }
+                    })
+                }
+                
+            }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
